35f295d06ddd6d59f306d2b77acdd2de470698a2
LucasDev <LucasDev9645@gmail.com>
2023-10-21 17:59:22 -0300

    Listing categories

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,11 +6,20 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+
 const router = Router();
 
 //-- Rotas User --/
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
+
+//-- Rotas Category --/
+router.post(
+  "/category",
+  isAuthenticated,
+  new CreateCategoryController().handle
+);
 
 export { router };
