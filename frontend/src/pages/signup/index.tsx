b35681d6915b2b48c7c175d79470ee40d84a6ea8
LucasDev <LucasDev9645@gmail.com>
2023-11-02 13:38:14 -0300

    Creation of the registration page

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -2,32 +2,33 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-import logoImg from "../../public/Logo.svg";
+import logoImg from "../../../public/Logo.svg";
 
-import styles from "../../styles/home.module.scss";
+import styles from "../../../styles/home.module.scss";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>SujeitoPizza - Cadastre-se</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo sujeito Pizzaria" />
 
         <div className={styles.login}>
           <form>
+            <Input placeholder="Nome da empresa" type="email" />
             <Input placeholder="Digite seu email" type="email" />
             <Input placeholder="Digite sua senha" type="password" />
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
-          <Link href="/signup" legacyBehavior>
-            <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          <Link href="/" legacyBehavior>
+            <a className={styles.text}>Já possuo uma conta</a>
           </Link>
         </div>
       </div>
