677f9d2bce6d87c4d422e1b8a10842098e8807e3
LucasDev <LucasDev9645@gmail.com>
2023-11-04 11:29:51 -0300

    Logging in

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, ReactNode, useState } from "react";
 import Router from "next/router";
-import { destroyCookie } from "nookies";
+import { destroyCookie, setCookie, parseCookies } from "nookies";
+
+import { api } from "../services/apiClient";
 
 type AuthContextData = {
   user: UserProps | undefined;
@@ -39,7 +41,33 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
 
-  async function signIn({ email, password }: SignInProps) {}
+  async function signIn({ email, password }: SignInProps) {
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+
+      const { id, name, token } = response.data;
+
+      setCookie(undefined, "@nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30,
+        patch: "/",
+      });
+
+      setUser({
+        id,
+        name,
+        email,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
