3b26cfacf41cda5604b678612bc7a9e8725d3854
LucasDev <LucasDev9645@gmail.com>
2023-11-10 18:59:41 -0300

    Component preview

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,11 +1,33 @@
+import { useState, ChangeEvent } from "react";
 import Head from "next/head";
 
 import { Header } from "@/src/components/Header";
 import { canSSRAuth } from "@/src/utils/canSSRAuth";
 
+import { FiUpload } from "react-icons/fi";
+
 import styles from "./styles.module.scss";
 
 export default function Product() {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) {
+      return;
+    }
+
+    const image = e.target.files[0];
+    if (!image) {
+      return;
+    }
+
+    if (image.type === "image/jpeg" || image.type === "image/png") {
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+    }
+  }
+
   return (
     <>
       <Head>
@@ -16,6 +38,25 @@ export default function Product() {
         <main className={styles.container}>
           <h1>Novo Produto</h1>
           <form className={styles.form}>
+            <label className={styles.labelAvatar}>
+              <span>
+                <FiUpload size={30} color="#fff" />
+              </span>
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={handleFile}
+              />
+              {avatarUrl && (
+                <img
+                  className={styles.preview}
+                  src={avatarUrl}
+                  alt="Foto do produto"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
             <select>
               <option>Bebidas</option>
               <option>Pizzas</option>
