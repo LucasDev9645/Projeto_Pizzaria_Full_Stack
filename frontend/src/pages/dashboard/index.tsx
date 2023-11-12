7c90477b290491e97038069dede9f045e4e941ff
LucasDev <LucasDev9645@gmail.com>
2023-11-12 09:45:16 -0300

    Registering dashboard, searching for orders

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -2,8 +2,31 @@ import { canSSRAuth } from "@/src/utils/canSSRAuth";
 import Head from "next/head";
 
 import { Header } from "@/src/components/Header";
+import { setupApiClient } from "@/src/services/api";
 
-export default function Dashboard() {
+import styles from "./styles.module.scss";
+import { FiRefreshCcw } from "react-icons/fi";
+import { useState } from "react";
+
+type OrdeProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+};
+
+interface HomeProps {
+  orders: OrdeProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps) {
+  const [orderList, setOrderList] = useState(orders || []);
+
+  function handleOpenModalView(id: string) {
+    alert("teste" + id);
+  }
+
   return (
     <>
       <Head>
@@ -11,14 +34,37 @@ export default function Dashboard() {
       </Head>
       <Header />
       <div>
-        <h1>Painel</h1>
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Útimos Pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#3fffa3" />
+            </button>
+          </div>
+          <article className={styles.listOrders}>
+            {orderList.map((item) => (
+              <section key={item.id} className={styles.orderItem}>
+                <button onClick={() => handleOpenModalView(item.id)}>
+                  <div className={styles.tag}></div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            ))}
+          </article>
+        </main>
       </div>
     </>
   );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiCliente = setupApiClient(ctx);
+
+  const response = await apiCliente.get("/orders");
+
   return {
-    props: {},
+    props: {
+      orders: response.data,
+    },
   };
 });
